ba9776e9d5e2d078a4bbe07753f9451ac59f3b2a
Roger Forner Fabre <hello@rogerforner.com>
2018-04-30 12:07:32 +0200

    ScoolProgramming ServiceProvider
    Creem el ServiceProvider del nostre paquet de la forma habitual de laravel, emprant el terminal.
    
    <php artisan make:provider ScoolProgrammingServiceProvider>

---
+++ app/Providers/ScoolProgrammingServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScoolProgrammingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
