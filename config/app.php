ac2835d479f8870ec4c57f9bf6f5ec1224a3300b
Roger Forner Fabre <hello@rogerforner.com>
2018-04-30 12:22:00 +0200

    [packProgramming] Registrar el Service Provider
    A continuació hem de registrar el ServiceProvider del nostre paquet a Laravel.

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rogerforner\ScoolProgramming\ScoolProgrammingServiceProvider::class,
 
         /*
          * Application Service Providers...
