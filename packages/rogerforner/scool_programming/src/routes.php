a26602c089d46455a1929ac89f2b51140d7d8f76
Roger Forner Fabre <hello@rogerforner.com>
2018-04-30 16:15:16 +0200

    [packProgramming] Routes
    Només hem de crear un sol fitxer per a les rutes. En aquest hi afegirem les de la API i les de la Web, tot i que aquestes últimes les emprarem, molt segurament, per fer alguna prova sobre el paquest; pot ser per a la landing page del paquet?

--- packages/rogerforner/scool_programming/src/routes.php
+++ packages/rogerforner/scool_programming/src/routes.php
@@ -12,3 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
