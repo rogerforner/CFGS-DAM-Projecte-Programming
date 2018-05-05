7999f4f3c83ac64c785e9ad8e25ac6dca28f44f0
Roger Forner Fabre <hello@rogerforner.com>
2018-05-05 19:52:36 +0200

    [packSocialite] configuració
    Google, amb la url que emprem en local no ens funcinarà.

--- config/services.php
+++ config/services.php
@@ -35,4 +35,40 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Socialite
+    |--------------------------------------------------------------------------
+    |   
+    | Create an App ID:
+    | - Facebook -> https://developers.facebook.com
+    | - GitHub   -> https://github.com/settings/developers
+    | - Google   -> https://console.developers.google.com/
+    | - Twitter  -> https://apps.twitter.com/
+    | 
+    |
+    */
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_APP_ID'),
+        'client_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect'      => env('FACEBOOK_REDIRECT'),
+    ],
+
+    'github' => [
+        'client_id'     => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect'      => env('GITHUB_REDIRECT')
+    ],
+
+    'google' => [
+        'client_id'     => env('GOOGLE_APP_ID'),
+        'client_secret' => env('GOOGLE_APP_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT')
+    ],
+
+    'twitter' => [
+        'client_id'     => env('TWITTER_APP_ID'),
+        'client_secret' => env('TWITTER_APP_SECRET'),
+        'redirect'      => env('TWITTER_REDIRECT')
+    ],
 ];
