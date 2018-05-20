b45f1213b10da500d25abd6c8035b4468a5509b0
Roger Forner Fabre <hello@rogerforner.com>
2018-05-20 19:47:08 +0200

    Preparant Sol·licituds per a Departments

--- resources/assets/js/api/department.js
+++ resources/assets/js/api/department.js
@@ -4,8 +4,30 @@
 import { ROAST_CONFIG } from '../config.js';
 
 export default {
-    // GET => /api/departments
-    getDepartments: function(){
-        return axios.get(ROAST_CONFIG.API_URL + '/departments');
+    // GET => /api/programming/dp
+    dpIndex: function(){
+        return axios.get(ROAST_CONFIG.API_URL + '/programming/dp');
+    },
+    // POST => /api/programming/dp
+    dpStore: function(name, description){
+        return axios.post(ROAST_CONFIG.API_URL + '/programming/dp', {
+            name: name,
+            description: description
+        });
+    },
+    // GET => /api/programming/dp/{id}
+    dpShow: function(id){
+        return axios.get(ROAST_CONFIG.API_URL + '/programming/dp/' + id);
+    },
+    // PUT => /api/programming/dp
+    dpUpdate: function(name, description, id){
+        return axios.put(ROAST_CONFIG.API_URL + '/programming/dp/' + id, {
+            name: name,
+            description: description
+        });
+    },
+    // DELETE => /api/programming/dp/{id}
+    dpDestroy: function(id){
+        return axios.delete(ROAST_CONFIG.API_URL + '/programming/dp/' + id);
     },
 }
---
+++ resources/assets/js/store.js
@@ -0,0 +1,15 @@
+/**
+ * Imports Vue and Vuex
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// Initializes Vuex on Vue.
+Vue.use(Vuex)
+
+// Exports our data store.
+export default new Vuex.Store({
+    modules: {
+
+    }
+});
