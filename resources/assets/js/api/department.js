c35a4d606e071eade19fd8560f54174444178512
Roger Forner Fabre <hello@rogerforner.com>
2018-05-20 19:06:26 +0200

    Preparant Sol·licituds + (vendor:publish + migrate)

---
+++ resources/assets/js/api/department.js
@@ -0,0 +1,11 @@
+/**
+ * Imports the Roast API URL from the config.
+ */
+import { ROAST_CONFIG } from '../config.js';
+
+export default {
+    // GET => /api/departments
+    getDepartments: function(){
+        return axios.get(ROAST_CONFIG.API_URL + '/departments');
+    },
+}
