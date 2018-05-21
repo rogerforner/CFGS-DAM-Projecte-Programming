dd66df1a0b7b97464626d4cce2fc43c525d22aa7
Roger Forner Fabre <hello@rogerforner.com>
2018-05-21 10:51:31 +0200

    Register + Login (social i app) OK

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('provider');
-            $table->string('provider_id');
-            $table->text('avatar');
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->text('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
