d45671c6d0cbd28ed6e26c21a2345a573dcb7780
Roger Forner Fabre <hello@rogerforner.com>
2018-05-28 23:27:22 +0200

    Favicon + Manifest

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -14,6 +14,15 @@
     <meta name="description" content="A Vue API SPA Scool Programming App.">
 
     <!-- WebSite: favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('icons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('icons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('icons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('icons/safari-pinned-tab.svg') }}" color="#007bff">
+    <link rel="shortcut icon" href="{{ asset('icons/favicon.ico') }}">
+    <meta name="msapplication-TileColor" content="#007bff">
+    <meta name="msapplication-config" content="{{ asset('icons/browserconfig.xml') }}">
+    <meta name="theme-color" content="#007bff">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
