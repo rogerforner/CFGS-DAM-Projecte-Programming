3475bed59a25de98cadb3faa03f62b3cb83e3b02
Roger Forner Fabre <hello@rogerforner.com>
2018-05-28 17:29:05 +0200

    Els usuaris amb rol Estudiant només poden accedir a la home (programacions PDF)

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,43 @@ Route::get('/', 'Web\WebController@getDashboard')->middleware('auth');
 
 Auth::routes();
 
-// Dashboard: Passport.
-Route::resource('dashboard/access-tokens', 'Web\AccessTokenController', [
-    'only' => ['index']
-]);
-
 // Dashboard: Home.
 Route::resource('dashboard', 'Web\DashboardController', [
     'only' => ['index']
 ]);
 
-// Dashboard: Programming, Departments.
-Route::resource('dashboard/departments', 'Web\DepartmentController', [
-    'only' => ['index']
-]);
+// En les següents rutes només podran accedir els/les Administradors/res i Professors/res.
+Route::middleware(['role:admin|teacher'])->group(function () {
+    // Dashboard: Passport.
+    Route::resource('dashboard/access-tokens', 'Web\AccessTokenController', [
+        'only' => ['index']
+    ]);
 
-// Dashboard: Programming, Professional Modules.
-Route::resource('dashboard/professional-modules', 'Web\ProfessionalModuleController', [
-    'only' => ['index']
-]);
+    // Dashboard: Programming, Departments.
+    Route::resource('dashboard/departments', 'Web\DepartmentController', [
+        'only' => ['index']
+    ]);
 
-// Dashboard: Programming, Temaries.
-Route::resource('dashboard/temaries', 'Web\TemaryController', [
-    'only' => ['index']
-]);
+    // Dashboard: Programming, Professional Modules.
+    Route::resource('dashboard/professional-modules', 'Web\ProfessionalModuleController', [
+        'only' => ['index']
+    ]);
 
-// Dashboard: Programming, Training Units.
-Route::resource('dashboard/training-units', 'Web\TrainingUnitController', [
-    'only' => ['index']
-]);
+    // Dashboard: Programming, Temaries.
+    Route::resource('dashboard/temaries', 'Web\TemaryController', [
+        'only' => ['index']
+    ]);
 
-// Dashboard: Programming, Users.
-Route::resource('dashboard/users', 'Web\UserController', [
-    'only' => ['index']
-]);
+    // Dashboard: Programming, Training Units.
+    Route::resource('dashboard/training-units', 'Web\TrainingUnitController', [
+        'only' => ['index']
+    ]);
+
+    // Dashboard: Programming, Users.
+    Route::resource('dashboard/users', 'Web\UserController', [
+        'only' => ['index']
+    ]);
+});
 
 // Si no estem autenticats serem enviats a la pàgina de login.
 // Des de aquesta escollirem amb quina xarxa social dur-lo a terme, passant el
